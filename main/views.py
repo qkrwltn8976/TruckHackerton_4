a19b0f3d4dcd39a86e01eb1ab189fdd9e04de287
qkrwltn8976 <qkrwltn8976@naver.com>
2019-11-09 19:42:32 +0900

    pages

--- main/views.py
+++ main/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'main/home.html')
+
+
+def select_game(request):
+    return render(request, 'main/select_game.html')
+
+
+def start_game(request):
+    return render(request, 'main/start_game.html')
 # Create your views here.
